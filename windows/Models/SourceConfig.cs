74fc8de43af25789b40dc4b7b7e4d220b8f2c704
jokers963 <120109967+jokers963@users.noreply.github.com>
2026-09-08 21:19:37 +0800

    Add Windows source configuration layer

---
+++ windows/Models/SourceConfig.cs
@@ -0,0 +1,8 @@
+namespace TVFongMi.Windows.Models;
+
+public sealed class SourceConfig
+{
+    public string Name { get; set; } = "默认源";
+    public string Url { get; set; } = "";
+    public bool Enabled { get; set; } = true;
+}
