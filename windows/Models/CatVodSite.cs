1b3110d7ac85db5722bb5b81a3a1d313ac06a6ea
jokers963 <120109967+jokers963@users.noreply.github.com>
2026-09-08 21:22:16 +0800

    Add CatVod configuration parsing models

---
+++ windows/Models/CatVodSite.cs
@@ -0,0 +1,10 @@
+namespace TVFongMi.Windows.Models;
+
+public sealed class CatVodSite
+{
+    public string Key { get; set; } = "";
+    public string Name { get; set; } = "";
+    public string Api { get; set; } = "";
+    public string Type { get; set; } = "";
+    public bool Searchable { get; set; } = true;
+}
